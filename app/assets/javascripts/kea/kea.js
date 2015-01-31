4c38edf861ea1ceac8c8cf7709a81b5ecff13549
Jan-Christian Föh <jan@programmanstalt.de>
2015-01-31 13:59:50 +0100

    Split into dependencies and kea itself for correct load order, include initializers

--- app/assets/javascripts/kea/kea.js
+++ app/assets/javascripts/kea/kea.js
@@ -1,25 +1,7 @@
-//= require knockout-3.2.0
-//= require ./kea_init
-
-//= require_directory ../../../vendor/assets/javascripts/.
-
-// Bower components
-
-//= require attachejs/attache
-//= require attachejs/attache-jquery
-//= require veiljs/veil
-//= require veiljs/veil-jquery
-//= require uri.js/src/URI.js
-//= require knockout-sortable/build/knockout-sortable
-//= require humane-js/humane
-//= require Keypress/keypress.js
-
-// Internal helpers and plugins
-
-//= require_directory ./helpers
-
 //= require_directory ./bindings
 //= require_directory ./extenders
 //= require ./models/base
 //= require ./services/base
 //= require ./viewmodels/base
+
+//= require ./initializers/base
---
+++ app/assets/javascripts/kea/kea_dependencies.js
@@ -0,0 +1,20 @@
+//= require knockout-3.2.0-debug
+
+//= require_directory ../../../../vendor/assets/javascripts/.
+
+// Bower components
+
+//= require attachejs/attache
+//= require attachejs/attache-jquery
+//= require veiljs/veil
+//= require veiljs/veil-jquery
+//= require uri.js/src/URI.js
+//= require knockout-sortable/build/knockout-sortable
+//= require humane-js/humane
+//= require Keypress/keypress.js
+
+//= require ./kea_init
+
+// Internal helpers and plugins
+
+//= require_directory ./helpers
