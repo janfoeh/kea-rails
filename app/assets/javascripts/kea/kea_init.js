6c678b0438528df4eb41a41cb7a66a88cb1892a3
Jan-Christian Föh <jan@programmanstalt.de>
2015-03-14 14:28:11 +0100

    Include more utilities

--- app/assets/javascripts/kea/kea_init.js
+++ app/assets/javascripts/kea/kea_init.js
@@ -20,5 +20,41 @@ window.kea.u = {
         configurable: true
       }
     });
+  },
+  
+  camelizeString: function camelizeString(string) {
+    "use strict";
+    
+    var string_parts = string.toLowerCase().split('_');
+    
+    for (var x = 0; x < string_parts.length; x++) {
+        string_parts[x] = string_parts[x].charAt(0).toUpperCase() + string_parts[x].substring(1);
+    }
+    
+    return string_parts.join('');
+  },
+  
+  underscoreString: function underscoreString(string) {
+    "use strict";
+    
+    var _regex_uppercase        = new RegExp('([A-Z])', 'g'),
+        _regex_underbar_prefix  = new RegExp('^_');
+          
+    string = string.replace(_regex_uppercase, '_$1');
+    string = string.replace(_regex_underbar_prefix, '');
+
+    return string.toLowerCase();
+  },
+  
+  dashString: function dashString(string) {
+    "use strict";
+    
+    var _regex_uppercase    = new RegExp('([A-Z])', 'g'),
+        _regex_dash_prefix  = new RegExp('^-');
+          
+    string = string.replace(_regex_uppercase, '-$1');
+    string = string.replace(_regex_dash_prefix, '');
+
+    return string.toLowerCase();
   }
 };
