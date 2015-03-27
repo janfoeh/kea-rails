a58243199974a04c11ff0a0a357a03385fb4cf80
Jan-Christian Foeh <jan@programmanstalt.de>
2015-03-27 17:54:46 +0100

    DatePicker binding

---
+++ app/assets/javascripts/kea/bindings/date_picker.js
@@ -0,0 +1,33 @@
+(function(ko, $) {
+  "use strict";
+
+  ko.bindingHandlers.datePicker = {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+      var $element      = $(element),
+          observable    = valueAccessor(),
+          options       = allBindingsAccessor().options || {},
+          defaultOptions;
+
+      defaultOptions = {
+        field: element,
+        firstDay: 1,
+        defaultDate: observable.date(),
+        format: observable.date.options.external,
+        onSelect: function() {
+          observable.date.fromMoment( this.getMoment() );
+        }
+      };
+
+      options = $.extend({}, defaultOptions, options);
+      
+      $element.data('pikaday', new Pikaday(options));
+    },
+    update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+      var observable  = valueAccessor(),
+          pikaday     = $(element).data('pikaday');
+          
+      pikaday.setMoment( observable.date.toMoment() );
+    }
+  };
+
+})(ko, $);
